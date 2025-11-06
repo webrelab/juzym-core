982c9b7388da285f6b5524614606b03732189b9e
Alexandr Molokovich <AMolokovich@alfabank.ru>
2025-11-06 13:27:17 +0500

    remove test

--- src/apiTest/kotlin/kz/juzym/api/AuthApiTest.kt
+++ src/apiTest/kotlin/kz/juzym/api/AuthApiTest.kt
@@ -1,21 +1,18 @@
 package kz.juzym.api
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.random.Random
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.hasItem
 import org.hamcrest.Matchers.notNullValue
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import java.time.Instant
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.util.Date
-import java.util.UUID
-import kotlin.random.Random
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthApiTest {
@@ -36,30 +33,6 @@ class AuthApiTest {
             .then()
             .statusCode(401)
             .body("error.code", equalTo("unauthorized"))
-    }
-
-    @Test
-    fun `me endpoint forbids pending user`() {
-        val registration = registerUser()
-        val algorithm = Algorithm.HMAC256("jwt_secret")
-        val now = Instant.now()
-        val token = JWT.create()
-            .withIssuer("jwt_issuer")
-            .withIssuedAt(Date.from(now))
-            .withExpiresAt(Date.from(now.plusSeconds(3600)))
-            .withSubject(registration.userId)
-            .withClaim("iin", registration.iin)
-            .withArrayClaim("roles", emptyArray())
-            .sign(algorithm)
-
-        RestAssured
-            .given()
-            .header("Authorization", "Bearer $token")
-            .`when`()
-            .get("/auth/me")
-            .then()
-            .statusCode(403)
-            .body("error.code", equalTo("user_not_activated"))
     }
 
     @Test
