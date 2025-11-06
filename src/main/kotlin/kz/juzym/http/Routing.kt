ca4e524461eca9a0edea7c464a87717b79d65115
Alexandr Molokovich <AMolokovich@alfabank.ru>
2025-11-06 11:08:09 +0500

    move health rout into api/v1

--- src/main/kotlin/kz/juzym/http/Routing.kt
+++ src/main/kotlin/kz/juzym/http/Routing.kt
@@ -1,10 +1,6 @@
 package kz.juzym.http
 
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
-import io.ktor.server.response.respond
-import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import kz.juzym.app.ApplicationContext
@@ -14,9 +10,6 @@ fun Application.registerRoutes(context: ApplicationContext) {
     routing {
         route("/api") {
             publicApiRoutes(context)
-        }
-        get("/health") {
-            call.respond(HttpStatusCode.OK, mapOf("status" to "ok"))
         }
     }
 }
